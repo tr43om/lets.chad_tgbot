3e2832effc36724bddf397c5e9163dddf04662f3
tr43om <tr43om@gmail.com>
2023-05-16 15:55:21 +0600

    initialized firebase

--- build/bot-webhook.js
+++ build/bot-webhook.js
@@ -1,6 +1,69 @@
-import { Telegraf } from "telegraf";
+import { Telegraf, session } from "telegraf";
 import config from "./config.js";
+import { message } from "telegraf/filters";
+import { ogg } from "./ogg.js";
+import { openai } from "./openai.js";
+import { code } from "telegraf/format";
+import { getMainMenu } from "./keyboards.js";
 export const bot = new Telegraf(config.telegramBotKey);
+bot.use(session());
+const INITIAL_SESSION = {
+    messages: [],
+};
+bot.start(async (ctx) => {
+    ctx.session = INITIAL_SESSION;
+    await ctx.replyWithHTML("Приветствую в <b>🗣️ giga.chat</b>\n\n", getMainMenu());
+    await ctx.replyWithSticker("CAACAgQAAxkBAANmZF0UULRUjhUT3-7_b9QdkF7OdJMAAsMjAAK18rhTnkVJ87ZCGMkvBA");
+});
+bot.command("stop", async (ctx) => {
+    ctx.session = INITIAL_SESSION;
+    await ctx.reply("кончил");
+});
+bot.on(message("voice"), async (ctx) => {
+    var _a;
+    (_a = ctx.session) !== null && _a !== void 0 ? _a : (ctx.session = INITIAL_SESSION);
+    try {
+        await ctx.reply(code("🔃 Принято! Сейчас отвечу..."));
+        // console.log(ctx.message);
+        await ctx.reply(JSON.stringify(ctx.message));
+        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+        const userId = String(ctx.message.from.id);
+        console.log(link.href);
+        const oggPath = await ogg.create(link.href, userId);
+        const mp3Path = await ogg.toMp3(oggPath, userId);
+        const prompt = await openai.transcription(mp3Path);
+        const { messages } = ctx.session;
+        ctx.reply(code(`🗣️ Ваш запрос: ${prompt}`));
+        messages.push({ role: "user", content: prompt });
+        const response = await openai.chat(messages);
+        messages.push({ role: "assistant", content: response.content });
+        await ctx.reply(response.content);
+    }
+    catch (error) {
+        console.log(`Error while voice message`, error.message);
+    }
+});
+bot.on(message("text"), async (ctx) => {
+    var _a;
+    (_a = ctx.session) !== null && _a !== void 0 ? _a : (ctx.session = INITIAL_SESSION);
+    try {
+        await ctx.reply(code("🔃 Принято! Сейчас отвечу..."));
+        const prompt = ctx.message.text;
+        const { messages } = ctx.session;
+        messages.push({ role: "user", content: prompt });
+        const response = await openai.chat(messages);
+        messages.push({ role: "assistant", content: response.content });
+        await ctx.reply(response.content);
+    }
+    catch (error) {
+        console.log(`Error while voice message`, error.message);
+    }
+});
+if (process.env.NODE_ENV === "development") {
+    bot.launch();
+}
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
 bot.on("message", async (ctx) => {
     await ctx.reply("Hi there!");
 });
---
+++ firebaseConfig.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCN3aICuPFMo28bNN_oJA7okNgFEiPpwcs",
+  authDomain: "letschadbot.firebaseapp.com",
+  projectId: "letschadbot",
+  storageBucket: "letschadbot.appspot.com",
+  messagingSenderId: "1010954075293",
+  appId: "1:1010954075293:web:16cb70292bd69a056e6874",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
